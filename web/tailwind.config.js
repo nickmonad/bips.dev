e616716761dc529ec217043e44d98e16b44063e4
nickmonad <nick@ngmiller.io>
2024-03-23 17:20:04 -0500

    improve local dev with tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,18 +1,13 @@
-const colors = require("tailwindcss/colors")
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./web/templates/**/*.html", "./web/content/**/*.md"],
   darkMode: 'selector',
   theme: {
-    colors: {
-        'gray': colors.gray,
-        'slate': colors.slate,
-        'zinc': colors.zinc,
-        'white': colors.white,
+    extend: {
+      colors: {
         'bitcoin': '#ff9900',
+      }
     },
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
